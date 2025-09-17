83d3f94aea41dc2296153ab38a961ae7ee35e367
Kabir-Sial <sial059689@gmail.com>
2025-09-17 06:24:34 +0500

    next js concept

---
+++ my_app/src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <main className="flex items-center justify-center m-5">
+
+   <h1 className="font-medium">Home Page</h1>
+
+   </main>
+   </>
+  );
+}
